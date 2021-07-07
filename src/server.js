a52ec7a3e20c4955a064e133914c616e231ce220
Emerson Teixeira <emerson.teixeira@mobapps.com.br>
2021-07-07 12:58:27 -0300

    aula 2

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3056
+const rotasUsuarios = require('./controller/usuario-controller')
+const rotasTarefas = require('./controller/tarefa-controller')
+
+rotasUsuarios(app)
+rotasTarefas(app)
+
+app.listen(port,()=>{
+    console.log('Servidor rodando na porta: '+ port)
+})
