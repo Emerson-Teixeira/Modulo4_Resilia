b2f9f65d40cbd9aafb9b1ff68055212903a9371e
Emerson Teixeira <emerson.teixeira@mobapps.com.br>
2021-07-07 19:59:14 -0300

    Aula 3

--- src/controller/usuario-controller.js
+++ src/controller/usuario-controller.js
@@ -1,5 +1,8 @@
 module.exports = (app) =>{
     app.get('/users',(req,res)=>{
-        res.send('Rota ativada com GET e recurso usuarios: valores de usuarios devem ser retornados')
+        res.send('Rota ativada com GET e recurso usuarios: valores de usuarios devem ser retornados!')
+    })
+    app.post('/users',(req,res)=>{
+        res.send('Rota POST de usuario ativada: usuário adicionado ao banco de dados!')
     })
 }
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+
 const port = 3056
 const rotasUsuarios = require('./controller/usuario-controller')
 const rotasTarefas = require('./controller/tarefa-controller')
