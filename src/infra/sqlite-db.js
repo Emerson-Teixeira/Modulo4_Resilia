8b108d5609ac0134e0d28f7774a3be83c2d9046f
Emerson Teixeira <emerson.teixeira@mobapps.com.br>
2021-07-20 08:11:48 -0300

    Revert "Merge branch 'main' of https://github.com/Emerson-Teixeira/Modulo4_Resilia into main"
    This reverts commit 4475bd82462aaee339d9444b35ea8818f30fd3b8, reversing
    changes made to 3f595ac87d84fa12aabb2bf014bbd28963f723b8.

--- src/infra/sqlite-db.js
+++ src/infra/sqlite-db.js
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
 
 const path = require('path');
 const caminhoArq = path.resolve(__dirname,'../','../','database.db')
 
 const sqlite3 = require('sqlite3').verbose();
 const bd = new sqlite3.Database(caminhoArq);
-=======
-const sqlite3 = require('sqlite3').verbose();
-const bd = new sqlite3.Database('./database.db');
-
->>>>>>> 0539bcb0272707594926b0798df9a1fee529708b
 
 //Processamento de sinal
 process.on('SIGINT', () =>
