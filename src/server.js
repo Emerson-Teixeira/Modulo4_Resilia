ed1d2bacd2ae0bb613e33113d7b94b76009e9a6b
Emerson Teixeira <emerson.teixeira@mobapps.com.br>
2021-07-26 18:50:18 -0300

    Porta pelo env

--- src/server.js
+++ src/server.js
@@ -2,5 +2,5 @@ const app = require('./app')
 
 //Listen
 app.listen(process.env.PORT,()=>{
-    console.log('Servidor rodando na porta: '+ process.env.PORT)
+    console.log('Servidor rodando na porta: '+ 3056)
 })
